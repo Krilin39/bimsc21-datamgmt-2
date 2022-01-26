2016477ef4e2aa075337164b73f23bca9aa63b95
Krilin39 <97433990+Krilin39@users.noreply.github.com>
2022-01-26 12:45:24 +1100

    Rhino Urban model added

--- example2/script.js
+++ example2/script.js
@@ -5,7 +5,7 @@ import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examp
 
 // declare variables to store scene, camera, and renderer
 let scene, camera, renderer
-const model = 'Rhino_Logo.3dm'
+const model = 'Sydney test1 mesh map reduced.3dm' //'Sydney test3 - LEP.3dm' //Sydney test1 mesh map.3dm' // 'Sydney test2 mesh.3dm' //'Rhino_Logo.3dm'
 
 // call functions
 init()
@@ -20,8 +20,11 @@ function init () {
     // create a scene and a camera
     scene = new THREE.Scene()
     scene.background = new THREE.Color(1,1,1)
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-    camera.position.y = - 30
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 )
+    //camera.position.y = - 30
+    camera.position.z = 1000;
+   
+
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer( { antialias: true } )
