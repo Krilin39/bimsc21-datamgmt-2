a4fd777e6ce8891e4dcd5105fcc3a3fa768d70cd
Krilin39 <97433990+Krilin39@users.noreply.github.com>
2022-01-26 15:10:10 +1100

    Top view

--- example3/script.js
+++ example3/script.js
@@ -17,8 +17,11 @@ function init() {
     // create a scene and a camera
     scene = new THREE.Scene()
     scene.background = new THREE.Color(1,1,1)
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-    camera.position.y = - 100
+    // camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+    // camera.position.y = - 100
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 )
+    //camera.position.y = - 30
+    camera.position.z = 1000;
 
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer( { antialias: true } )
