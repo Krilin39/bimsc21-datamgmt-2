c4315e67914f18a3cd19e967a654b164988350a4
Krilin39 <97433990+Krilin39@users.noreply.github.com>
2022-03-22 19:04:45 +1100

    A

--- example2/script.js
+++ example2/script.js
@@ -13,32 +13,25 @@ animate()
 
 // function to setup the scene, camera, renderer, and load 3d model
 function init () {
-
     // Rhino models are z-up, so set this as the default
     THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 )
-
     // create a scene and a camera
     scene = new THREE.Scene()
     scene.background = new THREE.Color(1,1,1)
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 )
     //camera.position.y = - 30
     camera.position.z = 1000;
-   
-
-
     // create the renderer and add it to the html
     renderer = new THREE.WebGLRenderer( { antialias: true } )
     renderer.setSize( window.innerWidth, window.innerHeight )
     document.body.appendChild( renderer.domElement )
-
     // add some controls to orbit the camera
     const controls = new OrbitControls( camera, renderer.domElement )
-
     // add a directional light
     const directionalLight = new THREE.DirectionalLight( 0xffffff )
-    directionalLight.intensity = 2
+    directionalLight.intensity = 3
     scene.add( directionalLight )
-
+    
     // load the model
     const loader = new Rhino3dmLoader()
     loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
